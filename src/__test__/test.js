7b0e5f693069998a29b7ea6db3a69bd7acc7979c
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-08-30 13:36:17 +0400

    adjustments eslint

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -4,8 +4,8 @@ test.each([
   ['healthy', 90, 'green'],
   ['wounded', 30, 'yellow'],
   ['critical', 9, 'red'],
-])
-('testint indicator function with %s condition and %i health', (condition, health, color) => {
+]) // eslint-disable-next-line
+('testint indicator function with %s condition and %i health', (condition, health) => {
   const result = conditionIndicator(health);
   expect(result).toBe(condition);
 });
